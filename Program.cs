9aa94595d4d11a23311d3f39ca56fe13d1901b47
Alex <sishoko@gmail.com>
2023-01-29 10:55:39 +0200

    Урок 1. Знакомство с языком программирования С# , Задача 2:

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.Clear();
+Console.WriteLine("Знакомство с языками программирования (семинары)");
+Console.WriteLine("Урок 1. Знакомство с языком программирования С#");
+
+Console.WriteLine("Input first number");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("-------------------Result-------------------");
+
+if (n1>n2)
+{
+    Console.WriteLine($"max = {n1} , min = {n2}");
+}
+else
+{
+    if (n1<n2)
+    {
+        Console.WriteLine($"max = {n2} , min = {n1}");
+    }
+    else
+        Console.WriteLine($"max = min = {n1}");
+}   
+Console.WriteLine("-------------------Result-------------------");
