5d3624d93528ad9bcec27e8312fd95ec44de553b
Ciobanu <Alexandru.Ciobanu@sistem.daac.md>
2023-03-09 16:02:41 +0200

    Урок 8. Двумерные массивы. Продолжение . Задача 61

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, 
-//добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
+﻿//Задача 61: Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
 
 
 
@@ -13,56 +7,55 @@ void PrepareProgramHeader()
 {
     Console.Clear();
     Console.WriteLine("Знакомство с языками программирования (семинары)");
-    Console.WriteLine("Урок 8. Двумерные массивы. Продолжение . Задача 60");
+    Console.WriteLine("Урок 8. Двумерные массивы. Продолжение . Задача 61");
 }
 
-int[,,] GetThreeDimensionalArray(int x, int y, int z)
+void PascalTriangles()
 {
-    int iterator = 10;
-    int[,,] array = new int[x, y, z];
-    Random random = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
+    Console.Write("Input rows number: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int[,] pascal = new int[n, n];
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j <= i; j++)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
+            if (j == 0 || j == i)
             {
-                array[i, j, k] = iterator;
-                iterator++;
+                pascal[i, j] = 1;
+            }
+            else
+            {
+                pascal[i, j] = pascal[i - 1, j - 1] + pascal[i - 1, j];
             }
         }
     }
 
-    return array;
-}
-
-void OutputArray(int[,,] array)
-//  в примере задачи как то странно выводит ... будь-то последовательность итереторва  1 и 2 перепутанны - тоесть  цикл бегает по одному измерению но выводит их в 
-// разной последовательности - уж не знаю это просто так получилось или с какой-то целью .
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int k = 0; k < n - i; k++)
         {
-            for (int k = 0; k < array.GetLength(1); k++)
-            {
-                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
-            }
-             Console.WriteLine();
+            Console.Write("  ");
         }
-       
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write("{0,4}", pascal[i, j]);
+        }
+
+        Console.WriteLine();
     }
 }
+
+
+
 
 
 // ---------------------- MAIN PROGRAM -------------------------
 PrepareProgramHeader();
-
-int[,,] array = GetThreeDimensionalArray(2, 2, 2);
-
 Console.WriteLine("-------------------Result-------------------\n");
-OutputArray(array);
-
+PascalTriangles();
 Console.WriteLine("\n-------------------Result-------------------");
 
 // ---------------------- MAIN PROGRAM -------------------------
